c89d26a7e5b821c6d1f5a644c112b8c19d31ec79
Sunanda71 <53290247+Sunanda71@users.noreply.github.com>
2025-11-28 21:10:19 +0530

    Create Practice.py

---
+++ Practice.py
@@ -0,0 +1,16 @@
+# Basic Python Practice File
+# Created by Sunanda
+
+# Print a message
+print("Hello, this is my first Python file on GitHub!")
+
+# Simple addition
+a = 10
+b = 20
+print("Sum:", a + b)
+
+# Simple function
+def greet(name):
+    return f"Hello {name}, welcome to Python practice!"
+
+print(greet("Sunanda"))
